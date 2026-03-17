b4020b27cd018638561e2da6c964ca1909880bea
SantosJGND <dourado.jns@gmail.com>
2026-03-17 11:05:19 +0000

    move generate default trees to register_televir_refs bash

--- components/insaflu-ubuntu/commands/register_televir_refs.sh
+++ components/insaflu-ubuntu/commands/register_televir_refs.sh
@@ -6,4 +6,5 @@
 #SBATCH --error=/data/tmp/register_televir_references_err.out
 
 
+/usr/bin/python3 /insaflu_web/INSaFLU/manage.py generate_default_trees
 /usr/bin/python3 /insaflu_web/INSaFLU/manage.py register_references_on_file --user_id 1 -o /tmp/insaFlu/register_references
--- components/insaflu-ubuntu/entrypoint.sh
+++ components/insaflu-ubuntu/entrypoint.sh
@@ -4,38 +4,38 @@ set -e
 
 
 move_slurm_key() {
-
+    
     MUNGE_KEY_SRC=/run/secrets/munge.key
     MUNGE_KEY_DST=/etc/munge/munge.key
-
+    
     if [ ! -s "$MUNGE_KEY_SRC" ]; then
-    echo "FATAL: munge.key missing"
-    exit 1
+        echo "FATAL: munge.key missing"
+        exit 1
     fi
-
+    
     # Ensure directory exists
     mkdir -p /etc/munge
-
+    
     # Copy only if needed
     if [ ! -f "$MUNGE_KEY_DST" ]; then
-    cp "$MUNGE_KEY_SRC" "$MUNGE_KEY_DST"
-    chown munge:munge "$MUNGE_KEY_DST"
-    chmod 400 "$MUNGE_KEY_DST"
+        cp "$MUNGE_KEY_SRC" "$MUNGE_KEY_DST"
+        chown munge:munge "$MUNGE_KEY_DST"
+        chmod 400 "$MUNGE_KEY_DST"
     fi
 }
 
 copy_slurm_config() {
-  if [ -d /run/secrets/slurm ]; then
-    echo "---> Initializing Slurm config from secrets"
-    cp -a /run/secrets/slurm/* /etc/slurm/
-
-    chown -R slurm:slurm /etc/slurm
-
-    # Strict permissions required by Slurm
-    chmod 640 /etc/slurm/slurm.conf || true
-    chmod 640 /etc/slurm/slurmdbd.conf || true
-    chmod 640 /etc/slurm/cgroup.conf || true
-  fi
+    if [ -d /run/secrets/slurm ]; then
+        echo "---> Initializing Slurm config from secrets"
+        cp -a /run/secrets/slurm/* /etc/slurm/
+        
+        chown -R slurm:slurm /etc/slurm
+        
+        # Strict permissions required by Slurm
+        chmod 640 /etc/slurm/slurm.conf || true
+        chmod 640 /etc/slurm/slurmdbd.conf || true
+        chmod 640 /etc/slurm/cgroup.conf || true
+    fi
 }
 
 # build DBs and launch frontend
@@ -58,46 +58,46 @@ if [ "$1" = "init_all" ]; then
         fi
         cp /insaflu_web/INSaFLU/env/insaflu.env /insaflu_web/INSaFLU/.env
     fi
-
+    
     ### set all default insaflu data
     echo "---> Collect static data  ..."
     cd /insaflu_web/INSaFLU; /usr/bin/python3 manage.py collectstatic --noinput;
     
     echo "---> Create/Update database if necessary  ..."
     cd /insaflu_web/INSaFLU; /usr/bin/python3 manage.py makemigrations; /usr/bin/python3 manage.py migrate;
-
+    
     ### set owners
     echo "---> Set owners APP_USER ..."
     chown -R APP_USER:slurm /insaflu_web/INSaFLU/media
     chown -R APP_USER:slurm /var/log/insaFlu
     ### This s for televir
     chown -R APP_USER:slurm /insaflu_web/INSaFLU/static_all
-
+    
     ### some files/paths are made by "root" account and need to be accessed by "flu_user"
     ### It's done by tmpfiles.d
     rm -rf /tmp/insaFlu/*
     if [ -d "/tmp/insaFlu" ]; then
         chmod -R 0777 /tmp/insaFlu
     fi
-
+    
     chmod -R 0777 /insaflu_web/INSaFLU/media
     chmod -R 0777 /insaflu_web/INSaFLU/static_all
     chmod -R 0777 /var/log/insaFlu
-
+    
     ### set default files and settings, deploy using slurm
     echo "---> Set default files and settings  ..."
     echo APP_USER
     mkdir -p /data/tmp && cd /data/tmp/;  chown -R APP_USER:slurm /data/;
     cp /insaflu_web/commands/load_defaults.sh .
-    gosu flu_user sbatch load_defaults.sh
+    sbatch load_defaults.sh
     cd /insaflu_web/INSaFLU;
-
+    
     ## for televir
     echo "--->  Set up TELEVIR software  ..."
     if [ -e /televir/mngs_benchmark/utility_docker.db ]; then
-        cd /insaflu_web/INSaFLU; /usr/bin/python3 manage.py generate_default_trees; #/usr/bin/python3 manage.py register_references_on_file -o /tmp/insaFlu/register
+        #/usr/bin/python3 manage.py register_references_on_file -o /tmp/insaFlu/register
         cd /data/tmp/ && cp /insaflu_web/commands/register_televir_refs.sh . && gosu flu_user sbatch register_televir_refs.sh
-        cd /insaflu_web/INSaFLU; 
+        cd /insaflu_web/INSaFLU;
     fi
     
     echo "---> Start apache server  ..."
