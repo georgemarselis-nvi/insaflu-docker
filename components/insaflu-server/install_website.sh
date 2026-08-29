f45727b684960de25e9ffc6dfe4a7c1051d68825
George Marselis <george@marsel.is>
2026-08-29 23:55:00 +0200

    fix: add python3-devel; restructure yum packages as array
    psycopg2 source build fails on missing Python.h: PyPI no
    longer ships python 3.6 wheels so pip compiles from source
    and the 3.6 headers were never installed (only
    python38-devel). Package list broken into one-per-line
    array for maintainability. Fixes INSaFLU/docker#38.

--- components/insaflu-server/install_website.sh
+++ components/insaflu-server/install_website.sh
@@ -6,7 +6,58 @@ sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /
 # Install package dependencies
 echo "Install package dependencies"
 yum -y install epel-release
-yum -y install gdal gdal-devel dos2unix parallel postgis postgresql-devel postgresql httpd httpd-tools httpd-devel mod_wsgi bash file binutils gzip git unzip wget java perl perl-devel perl-Time-Piece perl-XML-Simple perl-Digest-MD5 perl-CPAN perl-Module-Build perl-File-Slurp perl-Test* python38 python38-pip gcc zlib-devel bzip2-devel xz-devel python38-devel cmake cmake3 gcc-c++ autoconf bzip2 automake libtool which https://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/2.7.1/ncbi-blast-2.7.1+-1.x86_64.rpm
+# yum -y install gdal gdal-devel dos2unix parallel postgis postgresql-devel postgresql httpd httpd-tools httpd-devel mod_wsgi bash file binutils gzip git unzip wget java perl perl-devel perl-Time-Piece perl-XML-Simple perl-Digest-MD5 perl-CPAN perl-Module-Build perl-File-Slurp perl-Test* python38 python38-pip gcc zlib-devel bzip2-devel xz-devel python38-devel cmake cmake3 gcc-c++ autoconf bzip2 automake libtool which https://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/2.7.1/ncbi-blast-2.7.1+-1.x86_64.rpm
+
+packages=(
+    gdal
+    gdal-devel
+    dos2unix
+    parallel
+    postgis
+    postgresql-devel
+    postgresql
+    httpd
+    httpd-tools
+    httpd-devel
+    mod_wsgi
+    bash
+    file
+    binutils
+    gzip
+    git
+    unzip
+    wget
+    java
+    perl
+    perl-devel
+    perl-Time-Piece
+    perl-XML-Simple
+    perl-Digest-MD5
+    perl-CPAN
+    perl-Module-Build
+    perl-File-Slurp
+    "perl-Test*"
+    python38
+    python38-pip
+    python3-devel
+    gcc
+    zlib-devel
+    bzip2-devel
+    xz-devel
+    python38-devel
+    cmake
+    cmake3
+    gcc-c++
+    autoconf
+    bzip2
+    automake
+    libtool
+    which
+    https://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/2.7.1/ncbi-blast-2.7.1+-1.x86_64.rpm
+)
+
+yum -y install "${packages[@]}"
+
 if [ $? -ne 0 ]; then
     echo "Error installing system packages"
     exit 1
