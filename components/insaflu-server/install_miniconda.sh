0f82e0139b8e6b2cf9e4190c12e7fd65172218e4
George Marselis <george@marsel.is>
2026-08-30 23:10:45 +0200

    fix: pin pillow, matplotlib and treetime in Nextstrain Dengue env
    augur pulls phylo-treetime, which pulls matplotlib, which requires
    pillow>=8 unbounded. pillow 12.3.0 dropped the manylinux_2_17 wheel and
    ships only manylinux_2_27/2_28, so on glibc 2.17 pip falls back to the
    sdist. pillow 12 is C99 and gcc 4.8.5 defaults to gnu89.
    
    Pins installed before augur so pip resolves against them.
    
    Fixes INSaFLU/docker#50

--- components/insaflu-server/install_miniconda.sh
+++ components/insaflu-server/install_miniconda.sh
@@ -91,12 +91,16 @@ echo "Install Nextstrain Dengue"
 #conda create --name=nextstrain_dengue -c conda-forge mamba python=3.10 --yes && conda activate nextstrain_dengue && mamba install -c conda-forge -c bioconda mafft iqtree seqkit --yes && pip install nextstrain-cli nextstrain-augur snakemake && pip install pulp==2.7 && pip install epiweeks && curl -fsSL "https://github.com/nextstrain/nextclade/releases/download/3.10.2/nextclade-x86_64-unknown-linux-gnu" -o "/software/nextclade" && chmod +x /software/nextclade && mv /software/nextclade /software/miniconda2/envs/nextstrain_dengue/bin/ && ln -s /software/miniconda2/envs/nextstrain_dengue/bin/nextclade /software/miniconda2/envs/nextstrain_dengue/bin/nextclade3  && conda deactivate 
 # Pins: CentOS 7 has glibc 2.17. augur 23.1.1 constrains pandas/numpy/scipy to 1.*,
 # which still publish manylinux_2_17 wheels, so nothing is built from source.
-# See: https://github.com/INSaFLU/docker/issues/48
+# See:      https://github.com/INSaFLU/docker/issues/48
+# See also: https://github.com/INSaFLU/docker/issues/50
 install_nextstrain_dengue() {
     conda create --name=nextstrain_dengue -c conda-forge mamba python=3.10 --yes
     conda activate nextstrain_dengue
     mamba install -c conda-forge -c bioconda mafft iqtree seqkit --yes
     pip install nextstrain-cli==8.5.4
+    pip install pillow==12.2.0
+    pip install matplotlib==3.9.4
+    pip install phylo-treetime==0.11.5
     pip install nextstrain-augur==23.1.1
     pip install snakemake==7.32.2
     pip install cvxopt==1.3.2
